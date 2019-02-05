464876bcf268a3f6b6cb698e64ba53cfa7dd9922
DBotue <648237182@qq.com>
2019-02-05 13:11:05 +0800

    DB:116

--- first.cpp
+++ first.cpp
@@ -2467,6 +2467,39 @@
 //     return 0;
 // }
 
+/*116. Populating Next Right Pointers in Each Node*/
+// struct TreeLinkNode {
+//     int val;
+//     TreeLinkNode *left, *right, *next;
+//     TreeLinkNode(int x) : val(x), left(NULL), right(NULL), next(NULL) {}
+// };
+//
+// void connect(TreeLinkNode *root)
+// {
+//     if (root == nullptr)
+//         return ;
+//     if (root->left != nullptr)
+//         root->left->next = root->right;
+//     if (root->right != nullptr)
+//         root->right->next = (root->next!=nullptr) ? root->next->left : nullptr;
+//     connect(root->left);
+//     connect(root->right);
+// }
+//
+// int main()
+// {
+//     TreeLinkNode *root = new TreeLinkNode(1);
+//     root->left = new TreeLinkNode(2);
+//     root->right = new TreeLinkNode(3);
+//     root->left->left = new TreeLinkNode(4);
+//     root->left->right = new TreeLinkNode(5);
+//     root->right->left = new TreeLinkNode(6);
+//     root->right->right = new TreeLinkNode(7);
+//     connect(root);
+//     cout<<root->val<<endl;
+//     return 0;
+// }
+
 /*125. Valid Palindrome */
 //void toLower(string &s)
 //{
